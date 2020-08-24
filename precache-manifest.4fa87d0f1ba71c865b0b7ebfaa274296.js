d92e54e00d328e1d27c8c979d9bf435f7db1c340
virtu78 <virtu78@yandex.ru>
2020-08-24 13:48:19 +0500

    add dist subtree

---
+++ precache-manifest.4fa87d0f1ba71c865b0b7ebfaa274296.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e6840ce68012169f471",
+    "url": "/chucknorris/css/app.047db31a.css"
+  },
+  {
+    "revision": "7f6ea452d14fd5b50820612b5c2d11fb",
+    "url": "/chucknorris/img/chuck.7f6ea452.gif"
+  },
+  {
+    "revision": "01592f01d29d6bf7b6c5ab546236f9b4",
+    "url": "/chucknorris/index.html"
+  },
+  {
+    "revision": "0e6840ce68012169f471",
+    "url": "/chucknorris/js/app.418342e7.js"
+  },
+  {
+    "revision": "5fc64728111a372fa879",
+    "url": "/chucknorris/js/chunk-vendors.421218f2.js"
+  },
+  {
+    "revision": "b9541da30d1d360b6a3c613475806984",
+    "url": "/chucknorris/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/chucknorris/robots.txt"
+  }
+]);
